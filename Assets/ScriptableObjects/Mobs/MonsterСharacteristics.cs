679a0e2f0afde01dc12a5bb6ad9c2a5c5afb14e5
DmitriyKovriga <107250542+DmitriyKovriga@users.noreply.github.com>
2022-10-25 13:36:18 +0300

    Add DeepOne Scriptable Object
    Add DeepOne Scriptable Object

---
+++ Assets/ScriptableObjects/Mobs/MonsterСharacteristics.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mobs 
+{
+    [CreateAssetMenu(fileName = "New Monster Data", menuName = "Monster Characteristic Scriptable Object", order = 51)]
+    public class Monster—haracteristics : ScriptableObject
+    {
+        [SerializeField] private float _maxHp;
+        [SerializeField] private float _maxHpModificator;
+        [SerializeField] private float _attackDamage;
+        [SerializeField] private float _attackDamageModificator;
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _moveSpeedModificator;
+        [SerializeField] private float _dropBlood;
+        [SerializeField] private float _dropBloodModificator;
+        [SerializeField] private float _dropMoney;
+        [SerializeField] private float _dropMoneyModiificator;
+        [SerializeField] private float _stunTime;
+        [SerializeField] private float _stunResistance;
+        [SerializeField] private float _pushbackDistance;
+        [SerializeField] private float _pushbackResistance;
+
+       
+
+        public void setMaxHpModificator(float newValue)
+        {
+            _maxHp = newValue;
+        }
+
+        public float getMaxHpModificator()
+        {
+            return _maxHpModificator;
+        }
+
+        public void setAttackDamage(float newValue)
+        {
+            _attackDamage = newValue;
+        }
+
+        public float getAttackDamage()
+        {
+            return _attackDamage;
+        }
+
+        public void setAttackDamageModificator(float newValue)
+        {
+            _attackDamageModificator = newValue;
+        }
+
+        public float getAttackDamageModificator()
+        {
+            return _attackDamageModificator;
+        }
+
+        public void setMoveSpeed(float newValue)
+        {
+            _moveSpeed = newValue;
+        }
+
+        public float getMoveSpeed()
+        {
+            return _moveSpeed;
+        }
+
+        public void setMoveSpeedModificator(float newValue)
+        {
+            _moveSpeedModificator = newValue;
+        }
+
+        public float getMoveSpeedModificator()
+        {
+            return _moveSpeedModificator;
+        }
+
+        public void setDropBlood(float newValue)
+        {
+            _dropBlood = newValue;
+        }
+
+        public float getDropBlood()
+        {
+            return _dropBlood;
+        }
+
+        public void setDropBloodModificator(float newValue)
+        {
+            _dropBloodModificator = newValue;
+        }
+
+        public float getDropBloodModificator()
+        {
+            return _dropBloodModificator;
+        }
+
+        public void setDropMoney(float newValue)
+        {
+            _dropMoney = newValue;
+        }
+
+        public float getDropMoney()
+        {
+            return _dropMoney;
+        }
+
+        public void setDropMoneyModiificator(float newValue)
+        {
+            _dropMoneyModiificator = newValue;
+        }
+
+        public float getDropMoneyModiificator()
+        {
+            return _dropMoneyModiificator;
+        }
+
+        public void setStunTime(float newValue)
+        {
+            _stunTime = newValue;
+        }
+
+        public float getStunTime()
+        {
+            return _stunTime;
+        }
+
+        public void setStunResistance(float newValue)
+        {
+            _stunResistance = newValue;
+        }
+
+        public float getStunResistance()
+        {
+            return _stunResistance;
+        }
+
+        public void setPushbackDistance(float newValue)
+        {
+            _pushbackDistance = newValue;
+        }
+
+        public float getPushbackDistance()
+        {
+            return _pushbackDistance;
+        }
+
+        public void setPushbackResistance(float newValue)
+        {
+            _pushbackResistance = newValue;
+        }
+
+        public float getPushbackResistance()
+        {
+            return _pushbackResistance;
+        }
+    }
+}
+
